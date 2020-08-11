54774f7cefbbef01aa0290bbd075a6e4f97aa351
Vitali Semashkevich <vit-sema@yandex.ru>
2020-08-11 17:48:45 +0300

    Updates

---
+++ precache-manifest.c602f5bcb85351f6ff4059ae2492cf65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212b03597600e660f42583196eddc8d9",
+    "url": "/cap-store/index.html"
+  },
+  {
+    "revision": "34e4f251ebd0dde63075",
+    "url": "/cap-store/static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "34e4f251ebd0dde63075",
+    "url": "/cap-store/static/js/2.c31bcd16.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cap-store/static/js/2.c31bcd16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057fd97caef6e0738faa",
+    "url": "/cap-store/static/js/main.49b1a823.chunk.js"
+  },
+  {
+    "revision": "9f1fdf8e944f78311253",
+    "url": "/cap-store/static/js/runtime-main.135f12b7.js"
+  }
+]);
