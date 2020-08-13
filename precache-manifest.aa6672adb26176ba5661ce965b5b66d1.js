fc59a76da82721133f2c121451b5f35786e5fc42
Vitali Semashkevich <vit-sema@yandex.ru>
2020-08-13 19:17:56 +0300

    Updates

---
+++ precache-manifest.aa6672adb26176ba5661ce965b5b66d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4927f3f0b72292c8778191031cf34338",
+    "url": "/cap-store/index.html"
+  },
+  {
+    "revision": "aef264ec126d123f43e8",
+    "url": "/cap-store/static/css/2.fd3fe366.chunk.css"
+  },
+  {
+    "revision": "aef264ec126d123f43e8",
+    "url": "/cap-store/static/js/2.e7179165.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cap-store/static/js/2.e7179165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2a78b4da54dc4b9ca6b",
+    "url": "/cap-store/static/js/main.59f8d34e.chunk.js"
+  },
+  {
+    "revision": "9f1fdf8e944f78311253",
+    "url": "/cap-store/static/js/runtime-main.135f12b7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.681ce600e6f9c8c4f1fcaf317bd667b1.js"
+  "/cap-store/precache-manifest.aa6672adb26176ba5661ce965b5b66d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cap-store/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
